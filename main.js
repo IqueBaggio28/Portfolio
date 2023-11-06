cbaf2a18c31abf2a51180cb413a331397c4b4a25
Henrique Baggio <iquebaggio@gmail.com>
2023-11-06 00:33:30 -0500

    Made the 'burger' work + final changes to the proj

--- main.js
+++
@@ -1,3 +0,0 @@
-import mobileNav from './css/mobile-nav.js'; // Provide the correct relative path to the JavaScript file
-
-mobileNav(); // Call the mobileNav function if it's correctly defined in mobile-nav.js
--- mobile-nav.js
+++ mobile-nav.js
@@ -1,9 +1,36 @@
-const mobileNav = () => {
+const activateMobileNav = () => {
     const headerBtn = document.querySelector('.header__bars');
     const mobileNav = document.querySelector('.mobile-nav');
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+
+
+    // State
+    let isMobileNavOpen = false;
+
 
     headerBtn.addEventListener('click', () => {
-        mobileNav.style.display = 'flex';
+
+        isMobileNavOpen = !isMobileNavOpen;
+        if (isMobileNavOpen){
+            mobileNav.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';
+        }  
+        else {
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        }  
     });
+
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        })
+    })
+
+
+
 };
 
+activateMobileNav();
